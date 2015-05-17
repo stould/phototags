59232bfcb0837825fc0112027804903de9df6040
aajjbb <jefersonlsiq@gmail.com>
2015-05-16 22:40:54 -0300

    add

--- index.py
+++ index.py
@@ -170,7 +170,7 @@ def getphotos(username):
     
     response = httpGet("/v2.3/%s/photos?access_token=%s" % (user.userid, user.access_token)).decode("utf-8")
 
-    return response
+    return Parse.Parse(response, [""])
 
     
 @app.route('/parsedphotos/<username>')
